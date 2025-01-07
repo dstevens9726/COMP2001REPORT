e32d1abf293fc8191f83b9b3354f5ee287b8a093
dstevens9726 <dstevens9726@gmail.com>
2025-01-07 07:15:58 +0000

    new repo and file
    accidentally ruined the old file and it was easier to create a new repo and file and copy and paste over what wasnt broken. that is why the big initial commit

---
+++ COMP2001REPORT/__init__.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, app
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, migrate
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc://DStevens:EybZ719+@DIST-6-505.uopnet.plymouth.ac.uk/COMP2001_DStevens?driver=ODBC+Driver+17+for+SQL+Server"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+with app.app_context():
+    try:
+        db.engine.connect()
+        print(app.url_map)
+        print("Database Connected Successfully")
+    except Exception as e:
+        print("Error connecting to the database:", e)
+
+
+import models
+import procedures
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
